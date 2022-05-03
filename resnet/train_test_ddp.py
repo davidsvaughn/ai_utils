44cc7171dfd54a54fd8c19f96858b99787220bb5
davidsvaughn <davidsvaughn@gmail.com>
2022-05-03 14:50:46 -0400

    fixes

--- resnet/train_test_ddp.py
+++ resnet/train_test_ddp.py
@@ -354,9 +354,9 @@ def train_model(rank, args):
     ITEM, scale, fc, drops, print_every, rot, SEED  = 'Deteriorated', 1024, 256, [0.66,0.33], 300, 0.25, 191919
 
     cv_complete = True
-    K, alpha = 4, 0.25
+    K, alpha = 5, 0.25
     args.batch_size = 32
-    args.epochs = 5
+    args.epochs = 20
     args.res = 50
     
     #########################################
@@ -366,24 +366,23 @@ def train_model(rank, args):
     MODEL_PATH = os.path.join(MODEL_ROOT, ITEM)
     MODEL_FILE = os.path.join(MODEL_PATH, f'{ITEM}.pt')
     MODEL_CHKPT = os.path.join(MODEL_PATH, f'{ITEM}_chk.pt')
-    SAVE = False
-    if rank==0:
+    SAVE = True
+    if rank<1:
         make_dirs(MODEL_PATH)
 
     res, batch_size, N, LR = args.res, args.batch_size, args.freeze, args.lr
     Collate.scale, Collate.rot = scale, rot
 
     # set_random_seeds(random_seed=SEED)
-    if rank==0:
+    if rank<1:
         Ts,Ys,Ss = [],[],[]
         print(f'SEED={SEED}')
     
     for test_fold in range(K):
 
         set_random_seeds(random_seed=SEED)
-        # test_fold = K-1
 
-        if rank==0:
+        if rank<1:
             print(f'\nCV FOLD {test_fold+1}/{K}')
 
         ## load RESNET
@@ -400,19 +399,19 @@ def train_model(rank, args):
         ## freeze layers 1-N in the total 10 layers of Resnet
         ct = 0
         drop1, drop2 = drops
-        if rank==0 and test_fold==0:
+        if rank<1 and test_fold==0:
             print('MODEL:')
         for name, child in model.named_children():
             ct += 1
             if ct < N+1:
                 for param in child.parameters():
                     param.requires_grad = False
-                if rank==0 and test_fold==0:
+                if rank<1 and test_fold==0:
                     print(f'{ct} {name}\tFROZEN')
             else:
                 if drop1>0:
                     child.register_forward_hook(lambda m, inp, out: F.dropout(out, p=drop1, training=m.training))
-                if rank==0 and test_fold==0:
+                if rank<1 and test_fold==0:
                     print(f'{ct} {name}')
         
         ## customize output layer..
@@ -442,7 +441,7 @@ def train_model(rank, args):
 
         ## get paths...
         Collate.disable()
-        if rank==0:
+        if rank<1:
             pathmap = {}
             for _, _, paths, idx in testpathloader:
                 idx = idx.cpu().numpy()
@@ -466,11 +465,11 @@ def train_model(rank, args):
             trainloader.sampler.set_epoch(epoch)
             
             ## test metrics more frequently in later epochs....
-            # if epoch/args.epochs==0.25 :
-            #     pe /= 2
+            if epoch/args.epochs==0.5 :
+                pe /= 2
                 
             for inputs, labels, _,_ in trainloader:
-                frames += len(labels) * args.world_size
+                frames += args.batch_size * args.world_size
                 inputs, labels = inputs.to(rank), labels.to(rank)
                 
                 optimizer.zero_grad()
@@ -481,47 +480,47 @@ def train_model(rank, args):
                 running_loss += loss.item()
                 
                 if frames>=pe:
-                    train_imgs += frames
-                    train_secs += time.time()-t0
-                    t0, frames = time.time(), 0
 
-                    test_loss = 0
+                    if rank<1:
+                        t,y,p,test_loss = [],[],[],0
+                        train_imgs += frames
+                        train_secs += time.time()-t0
+                        t0 = time.time()
+
                     model.eval()
                     Collate.eval()
+                    frames = 0
 
                     with torch.no_grad():
-                        if rank==0:
-                            t,y,p = [],[],[]
 
                         for inputs, labels, _, idx in testloader:
-                            frames += len(labels) * args.world_size
                             inputs, labels, idx = inputs.to(rank), labels.to(rank), idx.to(rank)
-                            
                             logps = model.forward(inputs)
-                            batch_loss = criterion(logps, labels)
-                            test_loss += batch_loss.item()
                             ps = torch.exp(logps)
 
                             ## gather outputs from all DDP processes...
                             ps = gather_tensors(ps, device, rank, args.world_size)
-                            labels = gather_tensors(labels, device, rank, args.world_size)
+                            targets = gather_tensors(labels, device, rank, args.world_size)
                             idx = gather_tensors(idx, device, rank, args.world_size)
-                            if rank==0:
+
+                            if rank<1:
+                                frames += len(targets)
+                                batch_loss = criterion(logps, labels)
+                                test_loss += batch_loss.item()
                                 y.extend(ps[:,1].cpu().numpy())
-                                t.extend(labels.cpu().numpy())
+                                t.extend(targets.cpu().numpy())
                                 for i in idx.cpu().numpy():
                                     p.append(pathmap[i])
                     
                     ##################################################
+                    if rank<1:
+                        ## timings
+                        test_imgs += frames
+                        test_secs += time.time()-t0
+                        t0, frames = time.time(), 0
+                        fps_train = int(train_imgs/train_secs)
+                        fps_test = int(test_imgs/test_secs)
 
-                    test_imgs += frames
-                    test_secs += time.time()-t0
-                    t0, frames = time.time(), 0
-
-                    fps_train = int(train_imgs/train_secs)
-                    fps_test = int(test_imgs/test_secs)
-
-                    if rank==0:
                         update_model = False
                         t,y,p = np.array(t), np.array(y), np.array(p)
                         idx = np.argsort(-y)
@@ -562,23 +561,24 @@ def train_model(rank, args):
                             f"f1max: {ff1m:.3f} {scut} \t"
                             f"FPS:{fps_train}/{fps_test}"
                             )
-                    running_loss = 0
+                    ### end eval ##########
                     model.train()
                     Collate.train()
+                    frames, running_loss = 0,0
                     
             ## end epoch...
             #######################################################
-            if rank==0:
+            if rank<1:
                 print(f'{(time.time()-t1)/60:0.2f}min')
 
             if lr_sched is not None:
                 lr_sched.step()
-                if rank==0:
+                if rank<1:
                     print(f'lr={lr_sched.get_last_lr()[0]:0.3g}')
                 
         ## END TRAIN LOOP...
         ###########################################################
-        if rank==0:
+        if rank<1:
             Ts.extend(T)
             Ys.extend(Y)
             Ss.extend(S)
@@ -592,7 +592,7 @@ def train_model(rank, args):
     ## END K-FOLD LOOP...
     ###########################################################
     
-    if rank==0:
+    if rank<1:
         Ts,Ys,Ss = np.array(Ts), np.array(Ys), np.array(Ss)
         idx = np.argsort(Ys)
         Ts,Ys,Ss = Ts[idx],Ys[idx],Ss[idx]
